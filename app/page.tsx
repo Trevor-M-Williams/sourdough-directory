3e1967d2ab64e62f72bbe19ca424cceb56f98961
Trevor Williams <tmw7991@gmail.com>
2024-12-09 15:31:48 -0700

    fix import

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,27 @@
 import RecipeTable from "@/components/recipe-table";
-import { RecipeData } from "@/types";
-const data = require("@/data/recipes-data.json");
+import { RecipeData, RecipeWithIdAndCreatedOn } from "@/types";
+import { recipesData } from "@/data/recipes";
 
 export default async function Dashboard() {
   // const response = await fetch("http://localhost:3000/api/collection-items");
   // const data = await response.json();
 
-  const recipes = data.map((item: RecipeData) => ({
-    id: item.id,
-    name: item.fieldData.name,
-    category: item.fieldData.category,
-    createdOn: item.createdOn,
-    imageUrl: item.fieldData.image.url,
-  }));
+  const recipes: RecipeWithIdAndCreatedOn[] = recipesData.map(
+    (item: RecipeData) => ({
+      id: item.id,
+      name: item.fieldData.name,
+      category: item.fieldData.category,
+      slug: item.fieldData.slug,
+      ingredients: item.fieldData.ingredients,
+      instructions: item.fieldData.instructions,
+      prepTime: item.fieldData["prep-time-2"],
+      cookTime: item.fieldData["cook-time"],
+      servings: item.fieldData.servings,
+      calories: item.fieldData.calories,
+      imageUrl: item.fieldData.image.url,
+      createdOn: item.createdOn,
+    })
+  );
 
   return <RecipeTable recipes={recipes} />;
 }
